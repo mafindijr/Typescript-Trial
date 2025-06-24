081050fac42c5de75439ff1fbd1c6e2a4b442609
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-06-24 04:24:49 -0700

    fix: created two objects

--- basics.ts
+++ basics.ts
@@ -4,3 +4,25 @@ let myName: string = 'Bob';
 
 let numberOfWheels: number = 4;
 let isStudent: boolean = true;
+
+//custom types
+
+type person = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+let person = {
+
+    name: "joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2 = {
+
+    name: "jill",
+    age: 66,
+    isstudent: false
+}
