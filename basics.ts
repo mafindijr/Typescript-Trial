d7276a50b5b3c585727cd24186be0f6347f1ce23
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-03 01:20:55 -0700

    using the generics type to test how it works

--- basics.ts
+++ basics.ts
@@ -184,6 +184,6 @@ const gameScore = [14, 21, 33, 42, 59];
 const favoriteThings = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"];
 const voters = [{ name: "Alice", age: "42" }, { name: "Bob", age: 77 }];
 
-function getLastItem(array) {
+function getLastItem<PlaceholderType>(array: PlaceholderType[]) {
     return array[array.length - 1]
 }
