38d247c163fc5c3a430385f4d10db33bad259f0f
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-03 01:34:02 -0700

    fix: the funtion generics type

--- basics.ts
+++ basics.ts
@@ -184,7 +184,7 @@ const gameScore = [14, 21, 33, 42, 59];
 const favoriteThings = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"];
 const voters = [{ name: "Alice", age: "42" }, { name: "Bob", age: 77 }];
 
-function getLastItem<PlaceholderType>(array: PlaceholderType[]) {
+function getLastItem<PlaceholderType>(array: PlaceholderType[]): PlaceholderType | undefined {
     return array[array.length - 1]
 }
 
